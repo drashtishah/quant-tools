af7208e455b00f3870aaf17c937baa8ce7f124e1
Drashti Shah <drashti@inkredo.in>
2023-04-29 05:55:36 +0530

    Compute drawdown

--- quant-tools/with_dataframe.py
+++ quant-tools/with_dataframe.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from .constants import *
 
-def get_returns_from_prices(df):
+def get_returns_from_prices(df:pd.DataFrame) -> pd.DataFrame:
     '''Get returns from prices
     
     Input: Pandas DataFrame
@@ -17,7 +17,7 @@ def get_returns_from_prices(df):
     df = df.sort_index()
     return df.pct_change().dropna(how='all')
 
-def get_volatility_from_returns(df):
+def get_volatility_from_returns(df:pd.DataFrame) -> pd.Series:
     '''Get volatility from returns
     
     Input: Pandas DataFrame
@@ -30,7 +30,7 @@ def get_volatility_from_returns(df):
     '''
     return df.std()
 
-def get_annualized_volatility_from_returns(df):
+def get_annualized_volatility_from_returns(df:pd.DataFrame) -> pd.Series:
     '''Get annualized volatility from returns
     
     Input: Pandas DataFrame
@@ -43,7 +43,7 @@ def get_annualized_volatility_from_returns(df):
     '''
     return df.std()*np.sqrt(MONTHS_PER_YEAR)
 
-def get_compound_return_from_returns(df):
+def get_compound_return_from_returns(df:pd.DataFrame) -> pd.Series:
     '''Get compound return from returns per column
     
     Input: Pandas DataFrame
@@ -56,7 +56,7 @@ def get_compound_return_from_returns(df):
     '''
     return (df + 1).prod() - 1
 
-def get_annualized_return_from_returns(df):
+def get_annualized_return_from_returns(df:pd.DataFrame) -> pd.Series:
     '''Get annualized compound return from returns per column
     
     Input: Pandas DataFrame
@@ -69,3 +69,60 @@ def get_annualized_return_from_returns(df):
     '''
     number_of_periods = df.shape[0]
     return (df + 1).prod()**(MONTHS_PER_YEAR/number_of_periods) - 1
+
+def get_wealth_index_from_returns(df:pd.DataFrame, amount:float=1000.0) -> pd.DataFrame:
+    '''Get wealth index from returns
+    
+    Input: Pandas DataFrame
+     - date as index (must be datetime object)
+     - assets/stocks as columns
+     - returns as values
+    Output: Pandas DataFrame
+     - date as index
+     - assets/stocks as columns
+     - wealth index as values
+    '''
+    df = amount * (df + 1)
+    return df.cumprod()
+
+def get_previous_peaks_from_wealth_index(df:pd.DataFrame) -> pd.DataFrame:
+    '''Get previous peaks from wealth index
+    
+    Input: Pandas DataFrame
+     - date as index (must be datetime object)
+     - assets/stocks as columns
+     - wealth index as values
+    Output: Pandas DataFrame
+     - date as index
+     - assets/stocks as columns
+     - previous peaks as values
+    '''
+    return df.cummax()
+
+def get_drawdown_from_returns(df:pd.DataFrame, amount:float=1000.0) -> pd.DataFrame:
+    '''Get drawdown from returns
+    
+    Input: Pandas DataFrame
+     - date as index (must be datetime object)
+     - assets/stocks as columns
+     - returns as values
+    Output: Pandas DataFrame
+     - date as index
+     - assets/stocks as columns
+     - drawdown as values (not in %)
+    '''
+    wealth_index = get_wealth_index_from_returns(df, amount)
+    previous_peaks = get_previous_peaks_from_wealth_index(wealth_index)
+    return (wealth_index - previous_peaks) / previous_peaks
+
+def get_max_drawdown_from_returns(df:pd.DataFrame, amount:float=1000.0) -> pd.DataFrame:
+    '''Get maximum drawdown from returns
+    
+    Input: Pandas DataFrame
+     - date as index (must be datetime object)
+     - assets/stocks as columns
+     - returns as values
+    Output: Pandas DataFrame
+    '''
+    drawdown = get_drawdown_from_returns(df, amount)
+    return pd.DataFrame({'Date': drawdown.idxmin(), 'Maximum Drawdown': drawdown.min()})
